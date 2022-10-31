b197db043ad4d210ecfda4e5aa404cb5dc651116
Stephen <stephenasupe@gmail.com>
2022-10-31 07:59:06 -0500

    first commit

--- js/book.js
+++ js/book.js
@@ -1,14 +1,53 @@
-function Book(author, language, subject, title) {
+class Book {
+  constructor(author, language, subject, title) {
     this.author = author;
     this.language = language;
     this.subject = subject;
     this.title = title;
-  
-    /* CHANGE RENDER! THIS IS A TEMPLATE */
-    this.render = function () {
-      const li = document.createElement("li");
-      li.textContent = this.title;
-      return li;
-    };
   }
+
+
+render(){
+  //favorite button
+  const favoriteBtn = document.createElement('button');
+  favoriteBtn.innerHTML = 'add to list of favorites';
+  favoriteBtn.style.width ="15%" ;
+  // console.log(document);
+  const book = document.createElement('li');
+  // book.style.border = '1px', 'solid', 'black';
+  const bookInfo = document.createElement('ul');
   
+  const Title = document.createElement('li');
+  Title.innerHTML = `Title: ${this.title}`;
+  bookInfo.append(Title)
+  
+  const Author = document.createElement('li'); 
+  Author.innerHTML = `Author: ${this.author}`;
+  bookInfo.append(Author)
+
+  const Language = document.createElement('li');
+  Language.innerHTML = `Language: ${this.language}`;
+  bookInfo.append(Language)
+  const subjectwrapper = document.createElement('li');
+  const Subject = document.createElement('ul');
+        Subject.innerHTML = 'Subjects:'  
+  console.log(this.subject)  
+    for(const sub of this.subject){
+    const subElm = document.createElement('li');
+    subElm.innerHTML = sub;
+    Subject.append(subElm);
+    }
+    subjectwrapper.append(Subject)
+    bookInfo.append(subjectwrapper)
+    book.append(bookInfo)
+    book.append(favoriteBtn);
+  
+  return book;
+}
+
+
+
+}
+// let book1 = new Book('j.k. rowling', 'en', ['math', 'science'], 'harry potter' );
+// console.log(book1);
+// book1.render();
--- js/bookshelf.js
+++ js/bookshelf.js
@@ -1,17 +1,81 @@
-function Bookshelf(books = []) {
+class Bookshelf {
+  constructor(books = []) {
+    this.favorite = [];
     this.books = books;
+  }
   
-    this.addBook = function (book) {
+    addBook (book) {
       this.books.push(book);
     };
-  
-    /* CHANGE RENDER! THIS IS A TEMPLATE */
-    this.render = function () {
-      const ul = document.createElement("ul");
-      for (const book of this.books) {
-        ul.append(book.render());
+    addFavorite(book) {
+      this.favorite.push(book)
+    }
+    // create sort by number of topics/subjects
+    topic(){
+      console.log(this.topic) 
+      books.sort((a, b) => {
+        if(a.subject.length > b.subject.length) return 1; 
+        else {
+          return -1;}
+        })
+        this.books.push(this.topic);
       }
+    // render();
+  // create a sort alphabetically A-Z
+    alphabetAscend(){
+      this.books.sort((a, b) => {
+        if(a.title > b.title) return 1;
+        else {
+        return -1;}
+      })
+    }
+      // this.render();
+    }
+
+    alphabetDescend(){
+      this.books.sort((a, b) => {
+        if(a.title < b.title) return 1;
+        else {
+        return -1;}
+      })
+    }
+      this.render();
+    }
+    
+  
+ 
+    render () {
+      const ul = document.createElement("ul");
+  //refactor Bookshelf using map
+      this.books.map((book) => {
+        const newBook = new Book(book.author, book.language, book.subject, book.title)
+        ul.append(newBook.render());
+      const favoriteBtn = book.querySelector('button');
+      favoriteBtn.addEventListener('click', ()=> {
+        if(!this.favorite.includes(book)) this.addFavorite(book);
+      })
+      shelf.append(book);
+      })
+
+      console.log(ul)
       return ul;
     };
   }
-  
+  renderFav () {
+    const shelf = document.querySelector('#bookshelf');
+    shelf.innerHTML = '';
+    this.favorite.map((book) =>{ 
+      const book = book.render();
+      shelf.append(book);
+    })
+//create element with sum of favorite books displayed, use reduce
+const favCount = document.getElementById('#favcount');
+favCount.textContent = "Total Favorites:";
+const sumFav = books.reduce((p, c) =>{ p + 1
+}, 0); 
+return sumFav;
+favCount.append(sumFav);
+}
+
+
+ 
